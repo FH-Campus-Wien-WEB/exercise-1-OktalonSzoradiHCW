ea78a9d51a4903a77a068f303fe3194611e88047
OktalonSzoradi <oktalon.szoradi@stud.hcw.ac.at>
2026-03-17 17:09:34 +0100

    Separate CSS and JS from HTML
    Scripts should also be loaded at the end of <body> because they could potentially be blocking. See https://htmlhint.com/rules/head-script-disabled/ for more info.
    
    Additionally make an #app element which will be hooked into instead of the whole <body> element, like how Vue.js does it.

---
+++ server/files/js/main.js
@@ -0,0 +1,25 @@
+/* eslint-env browser */
+
+function reverseString (s) {
+  return s.split('').reverse().join('')
+}
+
+window.onload = function () {
+  const xhr = new XMLHttpRequest()
+  xhr.onload = function () {
+    const appElement = document.querySelector('#app')
+    if (xhr.status === 200) {
+      /* Part 2: Build the HTML elements here and append them to the body */
+      appElement.append(reverseString(xhr.responseText))
+    } else {
+      appElement.append(
+        'Daten konnten nicht geladen werden, Status ' +
+          xhr.status +
+          ' - ' +
+          xhr.statusText
+      )
+    }
+  }
+  xhr.open('GET', '/movies')
+  xhr.send()
+}
